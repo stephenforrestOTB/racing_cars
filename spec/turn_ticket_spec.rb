f3abfc78869766d9ea84cf52eb1008cfa6c9911c
Stephen Forrest <stephen.forrest@onthebeach.co.uk>
2017-10-10 09:57:05 +0100

    Initial commit - spec for one ticket dispenser

---
+++ spec/turn_ticket_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require 'turn_ticket'
+
+RSpec.describe TicketDispenser do
+  
+  context "dispenses a ticket" do
+    let(:ticket_zero) {subject.get_turn_ticket}
+    let(:ticket_one) {subject.get_turn_ticket}
+    it "returns a ticket object" do
+
+      expect(subject.get_turn_ticket.class).to eq(TurnTicket)
+    end
+    it "always returns a new ticket" do
+
+      expect(ticket_zero.object_id == ticket_one.object_id).to be(false)
+    end
+    it "always return a different number" do
+
+      expect(ticket_zero.turn_number).to_not eq(ticket_one.turn_number)
+    end
+    it "increments number by 1 each ticket" do
+      ticket_zero_id = ticket_zero.turn_number
+      ticket_one_id = ticket_one.turn_number
+
+      expect(ticket_one_id).to eq(ticket_zero_id + 1)
+    end
+  end
+end
