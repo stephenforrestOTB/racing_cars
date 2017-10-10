3b380e1047bc59efc5f77bd93b4bad7df4173ae7
Stephen Forrest <stephen.forrest@onthebeach.co.uk>
2017-10-10 10:38:24 +0100

    another way of doing it that doesn't violate Single Responsibility

--- lib/turn_ticket.rb
+++ lib/turn_ticket.rb
@@ -1,15 +1,31 @@
 class TurnTicket
-  attr_reader :ticket_number
+  attr_reader :turn_number
+
+  def initialize turn_number
+    @turn_number = turn_number
+  end
+end
+
+class TurnNumberSequence
 
   @@turn_number = -1
-  def initialize
+
+  def self.next_turn_number
     @@turn_number += 1
-    @ticket_number = @@turn_number
+
+    @@turn_number
   end
 end
 
 class TicketDispenser
+  def initialize(sequence_generator = TurnNumberSequence)
+    @sequence_generator = sequence_generator
+  end
   def get_turn_ticket
-    TurnTicket.new
+    TurnTicket.new(next_number)
+  end
+  private
+  def next_number
+    @sequence_generator.next_turn_number
   end
 end
